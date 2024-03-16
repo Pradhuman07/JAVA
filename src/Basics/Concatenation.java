cf99502cf5f46da43cabed85626230b0df2f2e80
Pradhuman07 <upradyuman@gmail.com>
2024-03-16 23:49:59 +0530

    New Files

---
+++ src/Basics/Concatenation.java
@@ -0,0 +1,8 @@
+package Basics;
+public class Concatenation {
+//    		--Role of + operator between String & numbers
+//			--String + String = String - Concatenation
+//			--String + int = String - Concatenation
+//			--int + int = int - Arithmetic Addition
+}
+
