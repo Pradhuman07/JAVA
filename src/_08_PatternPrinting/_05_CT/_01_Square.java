51062f0f6e9042cf5c8ba76d6fbd836ea8c67a9e
Pradhuman07 <upradyuman@gmail.com>
2024-05-16 23:29:47 +0530

    New Files

---
+++ src/_08_PatternPrinting/_05_CT/_01_Square.java
@@ -0,0 +1,19 @@
+package _08_PatternPrinting._05_CT;
+import java.util.Scanner;
+public class _01_Square {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter no. of Rows:-");
+        int n=sc.nextInt();
+
+        for(int i=0 ; i<n ; i++){
+
+            for(int j=0 ; j<n ; j++){
+                System.out.print("* ");
+            }
+            
+            System.out.println();
+        }
+    }
+}
---
+++ src/_08_PatternPrinting/_05_CT/_15_CraftedDiamond.java
@@ -0,0 +1,5 @@
+package _08_PatternPrinting._05_CT;
+public class _15_CraftedDiamond
+{
+    
+}
